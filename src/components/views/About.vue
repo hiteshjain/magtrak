07e8661a37b63a1b884d79fe3e43c8930538188f
JAINAM-PC\Priyanka <hiteshjain321@gmail.com>
2020-07-21 18:35:43 +0530

    about us

--- src/components/views/About.vue
+++ src/components/views/About.vue
@@ -1,5 +1,17 @@
 <template>
-    <h1>About</h1>
+  <div>
+    <v-container>
+      <v-row style="min-height: 300px" >
+        <v-col cols="12" sm="6">
+        </v-col>
+        <v-col cols="12" sm="6">
+          <h1 class="text-center primary--text bottom30">About Us</h1>
+          <p class="text-justify accent--text">MagTrak is marketed and distributed by <a href="http://adishwar.co.in/">Adishwar Telenetworks</a>. It is one of the leading manufacturer of precision locating equipments in India. Our product portfolio consist of various equipment which are used in building fiber optic cable networks. This consist of Fiber Jet – Fiber Optic Cable Blowing Machine, Fib Rod – Fiber Glass Duct Rodder used for cable pulling, HDPE duct accessories like Push Fit Couplers, Simplex Plugs, Manhole Bracket, End Plugs, etc.
+    Apart from manufacturing we also undertake fiber optic cable installation work, provide turnkey solution along with maintenance. Adishwar Tele Networks have the experience of successfully installing more than 70,000 km of Fiber Optic Cable networks in Indian and Middle East.</p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 <script>
 export default {
